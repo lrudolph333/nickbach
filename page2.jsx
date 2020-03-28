30ada041ee378a21e6befe775c2ed5fb3eb71e7e
lrudolph333 <trey.ru.iii@gmail.com>
2020-03-28 16:17:27 -0700

    Created React App

--- page2.jsx
+++ page2.jsx
@@ -1,2 +1,17 @@
 'use strict';
+import DeliveryTime from "page1.jsx"
 
+function Page2() {
+    return (
+        <div>
+            <DeliveryTime/>
+         </div>
+    )
+}
+
+//const header = <HeaderUpdate/>;
+const delivery = <DeliveryTime/>;
+const page = <Page2/>;
+
+const domContainer = document.getElementById('page2');
+ReactDOM.render(page, domContainer);
